b7f5135ab339a984d8ebe94c2a926589967548b5
Dave Vanderburg <dave@hitgrab.com>
2017-03-01 20:38:10 -0500

    Require.js and build config

---
+++ rjs/build.js
@@ -0,0 +1,13 @@
+({
+	appDir: "../www-src/",
+	baseUrl: "src",
+	dir: "../www",
+	mainConfigFile: '../www-src/src/main.js',
+	optimizeCss: "standard",
+	removeCombined: true,
+	preserveLicenseComments: false,
+	logLevel: 4,
+	modules: [
+		{ name: "main" },
+	]
+})
